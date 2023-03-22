2100ea633aeb4bb8f002444e8a4e9a0e2f2dabe0
Rama Hany <hanyrama703@gmail.com>
2023-03-22 15:57:39 -0700

    r

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,9 @@
 
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
- */
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+* followed by a new line
+*/
 	void print_alphabet_x10(void)
 	{
 		char ch;
@@ -21,4 +21,10 @@
 			{
 				_putchar(ch);
 				ch++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+
 
