43f1765d16fecd868a7c323a61662e05449a41bf
Rama Hany <hanyrama703@gmail.com>
2023-04-01 05:44:47 -0700

    g

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,14 @@
  */
 int main(void)
 {
-	for (int i = 'a'; i <= 'z'; i++)
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
 	{
-		putchar(i);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
 }
+
